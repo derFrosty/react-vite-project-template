a899f60b8ef18d80061380e14966bc580136d0c7
Frederico Gomes <frederico.94@hotmail.com>
2024-08-07 17:38:52 +0100

    add sass support

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import 'react-app-polyfill/stable';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './assets/styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
