d29ab930ab3a76bea04a0e24ea3eecfbd261d8fb
Frederico Gomes <frederico.94@hotmail.com>
2024-08-11 11:37:04 +0100

    install and apply eslint rules

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 import './assets/styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
